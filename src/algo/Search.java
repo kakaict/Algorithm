66481d272fbb98a711c733056f9cbf81af94e445
quangkhai28 <quangkhai.tran.fr@gmail.com>
2017-05-13 12:17:29 +0200

    edit searhc

--- src/algo/Search.java
+++ src/algo/Search.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
 public class Search<T> {
 
 	public boolean searchBinary(List<T> list, T key, Comparator<T> c){
