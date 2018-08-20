4681416081047053b338fedc78ec8e633c814eda
kakaict <canhcung88@gmail.com>
2018-08-21 01:12:27 +0200

    up

--- src/amazon/TripletArray.java
+++ src/amazon/TripletArray.java
@@ -2,12 +2,14 @@ package amazon;
 
 import java.util.Arrays;
 
+import utils.ArrayUtils;
+
 /**
 *
 * @author khaitq on 19 août 2018
 * 
 * Given an array of integers, write a function that find all  
-* triplet (a, b, c) that satisfies a + b + c = 0 .
+* triplet (a, b, c) that satisfies a + b + c =  v .
 * 
 * Solution 1: O(n3)
 * 
@@ -19,9 +21,9 @@ public class TripletArray {
 	public static void main(String[] args) {
 		
 		int[] arr  =  { 130, 11, 13, 5, 6, 20, 9, 100, 1, 3 , 10 , 0 };
-		int  v = 3;
+		int  v = 23;
 		
-		System.out.print(isTripletArray(arr, 20));
+		System.out.print(isTripletArray(arr, v));
 	}
 	
 	public static boolean isTripletArray(int[] arr, int v) {
@@ -29,19 +31,21 @@ public class TripletArray {
 		boolean result = false; 
 		Arrays.sort(arr);
 		
+		ArrayUtils.printArray(arr);
+		
 		int i = 0; 
 		int j = arr.length - 1;
 		
 		while (i + 1 < j) {
 			
-			boolean f = false;
-			
 			if (arr[i] + arr[j] < v) {
 				
-				for (int k = i; k < j; k++) {
+				boolean f = false;
+				
+				for (int k = i + 1; k < j; k++) {
 					
 					if (arr[j] + arr[i] + arr[k] == v) {
-						System.out.println(arr[j] + " - " +   arr[i] + " - " + arr[k]);
+						System.out.println(arr[i] + " - " +   arr[k] + " - " + arr[j]);
 						f = true;
 						result = true;
 					}
