4ef970960b9414d625b9f93b977928ff2c09d750
kakaict <canhcung88@gmail.com>
2018-09-09 18:55:05 +0200

    up

--- src/recusive/ReverseDigit.java
+++ src/recusive/ReverseDigit.java
@@ -1,4 +1,4 @@
-package rercusive;
+package recusive;
 
 
 /**
---
+++ src/recusive/string/BuildStringFromCharaters.java
@@ -0,0 +1,57 @@
+package recusive.string;
+
+import java.util.List;
+
+/**
+ * Created by khaitq
+ * Date: 09/09/2018
+ * Github:  https://github.com/quangkhai88
+ */
+
+
+/*
+    Print all possible strings of length k that can be formed from a set of n characters
+
+    set[] = {'a', 'b'}, k = 2
+    Output: aa bb ab ba
+ */
+
+public class BuildStringFromCharaters {
+
+    public static  void  main(String args[] ) {
+
+        char [] set = { 'a', 'b' , 'c' };
+        int n = 3;
+
+
+        print("", set, n);
+
+    }
+
+    public static void print(String prefix, char [] set, int n) {
+
+        if (prefix.length() == n) {
+
+            System.out.println(prefix);
+
+        } else {
+
+            for (int i = 0; i < set.length; i ++) {
+
+                print(prefix + set[i], set, n);
+
+            }
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
