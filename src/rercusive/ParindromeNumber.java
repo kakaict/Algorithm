b4c09540c1436c60fda6238c40cbe9d672332ca7
kakaict <canhcung88@gmail.com>
2018-09-09 12:40:07 +0200

    up

--- src/rercusive/ParindromeNumber.java
+++ src/rercusive/ParindromeNumber.java
@@ -10,7 +10,9 @@ public class ParindromeNumber {
 
 	public static void main(String[] args) {
 
-		
+		System.out.println(isParindromeNumber(12321));
+
+        System.out.println(isParindromeNumber(20502));
 		
 	}
 	
@@ -18,6 +20,33 @@ public class ParindromeNumber {
 	/*
 	 * 24542 is a parindrome number : x = reverse(x)
 	 */
+
+	public static boolean isParindromeNumber(int x) {
+
+		if (x < 10)
+			return true;
+
+		if (x < 100)
+			return x/10 == x %10;
+
+		int a = x % 10;
+		int b = x /10;
+		int i = 10;
+		while (b > 10) {
+		    b = b /10;
+            i = i * 10;
+		}
+
+        if (b != a)
+            return false;
+
+		x = (x - a) - b * i;
+		x = x /10;
+		while (x % 10 == 0)
+            x /= 10;
+
+        return isParindromeNumber(x);
+	}
 
 }
 
